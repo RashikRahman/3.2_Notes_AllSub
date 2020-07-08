8b6cdbe2fc731567d50096745ab45893ffaecee5
Rashik_Rahman_Pritom <57052944+RashikRahman@users.noreply.github.com>
2020-07-09 01:57:13 +0600

    update

---
+++ CSE_316_Peripheral & Interfacing Lab/sketch_jul08a/sketch_jul08a.ino
@@ -0,0 +1,13 @@
+void setup() {
+  pinMode(13,OUTPUT); //(pin number, status [OUTPUT or INPUT]
+}
+
+// the loop routine runs over and over again forever:
+void loop() {
+  digitalWrite(13,HIGH);  //(pin number, Mode=[HIGH=1, 5v or LOW=0, 0v])
+  delay(1000);
+  digitalWrite(13,LOW);  //(pin number, Mode=[HIGH=1, 5v or LOW=0, 0v])
+  delay(1000);
+  
+
+}
