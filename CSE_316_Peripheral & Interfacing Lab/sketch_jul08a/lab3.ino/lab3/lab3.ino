6b8f74cc6155d51fc0db7df0a93a15e0756437d9
Rashik_Rahman_Pritom <57052944+RashikRahman@users.noreply.github.com>
2020-07-26 02:33:58 +0600

    week4

--- CSE_316_Peripheral & Interfacing Lab/sketch_jul08a/lab3.ino/lab3/lab3.ino
+++ CSE_316_Peripheral & Interfacing Lab/sketch_jul08a/lab3.ino/lab3/lab3.ino
@@ -1,8 +1,6 @@
 int led=11;
 void setup() {
-  // put your setup code here, to run once:
-//  pinMode(led,OUTPUT);//(pin number, Value)
-  //analogWrite();
+  //  pinMode(led,OUTPUT);//(pin number, Value)
   Serial.begin(9600);
   Serial.print(78);
   Serial.println();
@@ -21,16 +19,14 @@ void setup() {
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
-//  for(int fade_value=0; fade_value<=255;fade_value+=5)
-//  {
-//    analogWrite(led,fade_value);
-//    delay(1000);
-//  }
+  //  for(int fade_value=0; fade_value<=255;fade_value+=5)
+    //  {
+    //    analogWrite(led,fade_value);
+    //    delay(1000);
+    //  }
 //  for(int fade_value1=255;fade_value1>=0;fade_value1-=5)
-//  {
-//    analogWrite(led,fade_value1);
-//    delay(1000);
-//  }
+    //  {
+    //    analogWrite(led,fade_value1);
+    //    delay(1000);
+    //  }
 }
